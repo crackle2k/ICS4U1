bcb00673faed6f6208e1f3fecc97b73cce2f3be0
Crackle2K <103861462+Crackle2K@users.noreply.github.com>
2026-09-09 16:29:35 -0500

    build: Initial commit

---
+++ src/net/dinesh/unit1/lesson1/Q6.java
@@ -0,0 +1,11 @@
+package net.dinesh.unit1.lesson1;
+
+public class Q6 {
+    public static void main(String[] args) {
+        System.out.println("  / \\__      ");
+        System.out.println(" (    @\\___");
+        System.out.println(" /         O  Hello Java Coder!");
+        System.out.println("/   (_____/ ");
+        System.out.println("/_____/   U");
+    }
+}
---
+++ src/net/dinesh/unit1/lesson1/Q7.java
@@ -0,0 +1,7 @@
+package net.dinesh.unit1.lesson1;
+
+public class Q7 {
+    public static void main(String[] args) {
+        System.out.println("   *\n  ***\n *****\n*******\n *****\n  ***\n   *");
+    }
+}
